6a603228d0896b7d726ab1bd22166d3da1af3345
gkalstn000 <gkalstn000@gmail.com>
2023-05-04 12:52:47 +0900

    default lr 0.0001

--- options/train_options.py
+++ options/train_options.py
@@ -25,7 +25,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--optimizer', type=str, default='adam')
         parser.add_argument('--beta1', type=float, default=0.0, help='momentum term of adam')
         parser.add_argument('--beta2', type=float, default=0.999, help='momentum term of adam')
-        parser.add_argument('--lr', type=float, default=0.0004, help='initial learning rate for adam')
+        parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate for adam')
         parser.add_argument('--no_TTUR', action='store_true', help='Use TTUR training scheme')
         # the default values for beta1 and beta2 differ by TTUR option
         opt, _ = parser.parse_known_args()
