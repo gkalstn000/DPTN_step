a0b79234717010a22a50d75e4ad6d58eb640d54a
gkalstn000 <gkalstn000@gmail.com>
2023-05-28 18:51:25 +0900

    TODO * Generator input: 16x16, output: 16x16 * Generator 입력 전 pose encoding * ResNet Decoder 입력: 16x16, 출력: 256x256 * step 만들기. * Pose load 할 때 16x16 으로 해보기.

--- models/dptn_networks/decoder.py
+++ models/dptn_networks/decoder.py
@@ -3,61 +3,52 @@ import torch.nn as nn
 from models.dptn_networks import modules
 from models.dptn_networks.base_network import BaseNetwork
 from models.spade_networks.architecture import SPADEResnetBlock, SPAINResnetBlock
+from models.spade_networks.normalization import get_nonspade_norm_layer
 
 import torch.nn.functional as F
+
+class Resblock(nn.Module) :
+    def __init__(self, opt, input_nc, output_nc):
+        super(Resblock, self).__init__()
+        self.opt = opt
+
+        kw = 3
+        norm_layer = get_nonspade_norm_layer(opt, opt.norm_G)
+
+        self.conv1 = norm_layer(nn.Conv2d(input_nc, output_nc, kernel_size=3, stride=1, padding=1))
+        self.conv2 = norm_layer(nn.Conv2d(output_nc, output_nc, kernel_size=3, stride=1, padding=1))
+        self.shortcut = norm_layer(nn.Conv2d(input_nc, output_nc, kernel_size=3, stride=1, padding=1))
+    def forward(self, x):
+        out = self.conv2(self.conv1(x)) + self.shortcut(x)
+        return out
 
 class ImageDecoder(nn.Module) :
     def __init__(self, opt):
         super(ImageDecoder, self).__init__()
         self.opt = opt
         nf = opt.ngf
-        norm_nc = opt.pose_nc
 
-        self.sw, self.sh = self.compute_latent_vector_size(opt)
+        self.layer1 = Resblock(opt, 1, 16 * nf)
+        self.layer2 = Resblock(opt, 16 * nf, 8 * nf)
+        self.layer3 = Resblock(opt, 8 * nf, 4 * nf)
+        self.layer4 = Resblock(opt, 4 * nf, 2 * nf)
 
-        self.fc = nn.Linear(opt.z_dim, 16 * nf * self.sw * self.sh)
-
-        self.head_0 = SPADEResnetBlock(16 * nf, 16 * nf, opt, norm_nc)
-        self.G_middle_0 = SPADEResnetBlock(16 * nf, 16 * nf, opt, norm_nc)
-        self.G_middle_1 = SPADEResnetBlock(16 * nf, 16 * nf, opt, norm_nc)
-
-        self.up_0 = SPADEResnetBlock(16 * nf, 8 * nf, opt, norm_nc)
-        self.up_1 = SPADEResnetBlock(8 * nf, 4 * nf, opt, norm_nc)
-        self.up_2 = SPADEResnetBlock(4 * nf, 2 * nf, opt, norm_nc)
-        self.up_3 = SPADEResnetBlock(2 * nf, 1 * nf, opt, norm_nc)
-
-        final_nc = nf
-
-        if opt.num_upsampling_layers == 'most':
-            self.up_4 = SPADEResnetBlock(1 * nf, nf // 2, opt, norm_nc)
-            final_nc = nf // 2
+        final_nc = 2 * nf
 
         self.conv_img = nn.Conv2d(final_nc, 3, 3, padding=1)
 
         self.up = nn.Upsample(scale_factor=2)
 
 
-    def forward(self, texture_param, pose_information):
-        mu, var = texture_param
-        z = self.reparameterize(mu, var)
-
-        x = self.fc(z)
-        x = x.view(-1, 16 * self.opt.ngf, self.sh, self.sw)
-
-        x = self.head_0(x, pose_information)
-
-        x = self.up(x)
-        x = self.G_middle_0(x, pose_information)
-        x = self.G_middle_1(x, pose_information)
-
-        x = self.up(x)
-        x = self.up_0(x, pose_information)
-        x = self.up(x)
-        x = self.up_1(x, pose_information)
-        x = self.up(x)
-        x = self.up_2(x, pose_information)
-        x = self.up(x)
-        x = self.up_3(x, pose_information)
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.up(x) # 16x16 -> 32x32
+        x = self.layer2(x)
+        x = self.up(x) # 32x32 -> 64x64
+        x = self.layer3(x)
+        x = self.up(x) # 64x64 -> 128x128
+        x = self.layer4(x)
+        x = self.up(x) # 128x128 -> 256x256
 
         x = self.conv_img(F.leaky_relu(x, 2e-1))
         x = F.tanh(x)
